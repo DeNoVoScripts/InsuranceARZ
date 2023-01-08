ac2959720b47e423ac9e15865c225ca250ade397
DeNoVoScripts <122185695+DeNoVoScripts@users.noreply.github.com>
2023-01-08 03:41:03 +0300

    Update MedicalHelper.lua

--- MedicalHelper.lua
+++ MedicalHelper.lua
@@ -1,7 +1,7 @@
 script_name("MedicalHelper")
 script_authors("Christian Costa")
 script_description("Script for the Ministries of Health Arizona Saint Rose")
-script_version("2.5.38")
+script_version("2.5.48")
 script_properties("work-in-pause")
 setver = 1
  
